7b35dfb5267441f5e0c62f5b279baac99f98f26c
ulvikrml <ulvikrml@gmail.com>
2022-07-31 13:57:36 +0400

    v1.5.1 'cart'

--- main.js
+++ main.js
@@ -110,7 +110,7 @@ if (trendContainer) {
         <span class="old-price">${element.old_price}</span>` : ``;
 
         data.innerHTML = `
-            <div class="swiper-slide">
+            <div class="swiper-slide product-card" data-id="${element.id}">
                 <div class="card-photo">
                     <img src="img/trend/${element.image}.jpg" alt="${element.image}">
                 </div>
@@ -134,8 +134,8 @@ if (trendContainer) {
                     <button class="trend-add addToCartBtn">ADD TO CARD</button>
                 </div>
                 <div class="adds">
-                  <div class="add-box quick-view-btn" data-id="${element.id}" onClick="quickView(event)"><i class="far fa-eye trendcard-add"></i></div>
-                  <div class="add-box add-to-wishlist" data-id="${element.id}"><i class="far fa-heart trendcard-add"></i></div>
+                  <div class="add-box quick-view-btn" onClick="quickView(event)"><i class="far fa-eye trendcard-add"></i></div>
+                  <div class="add-box add-to-wishlist"><i class="far fa-heart trendcard-add"></i></div>
                </div>
             </div>
     `
@@ -148,7 +148,7 @@ if (trendContainer) {
 const quickView = (e) => {
     console.log(e);
     console.log(e.target);
-    const closest = e.target.closest(".quick-view-btn");
+    const closest = e.target.closest(".product-card");
     console.log(products[closest.dataset.id]);
     const product = products.find((item) => item.id == closest.dataset.id);
     showViewModal(product)
@@ -227,19 +227,19 @@ const showViewModal = (data) => {
 //     WISHLIST
 let wishList = [];
 wishList = JSON.parse(localStorage.getItem('wishList'));
+wishList = wishList == null ? [] : wishList;
+wishList = wishList[0] == null ? [] : wishList;
 
 //    adding to wishlist
 const addToWishlistBtn = document.querySelectorAll('.add-to-wishlist');
 addToWishlistBtn.forEach(element => {
     element.addEventListener('click', (e) => {
-        const closest = e.target.closest(".add-to-wishlist");
-        // const product = products[closest.dataset.id];
+        const closest = e.target.closest(".swiper-slide");
         const product = products.find((item) => item.id == closest.dataset.id);
         const even = (element) => element.id == product.id;
-        wishList = wishList[0] == null ? [] : wishList;
+        // wishList = wishList[0] == null ? [] : wishList;
         const isActive = wishList.some(even);
         isActive ? '' : wishList.push(product);
-        // console.log(wishList);
         localStorage.setItem('wishList', JSON.stringify(wishList));
         updateWishlistCount(wishList.length);
         console.log(wishList);
@@ -252,9 +252,13 @@ const delItemWishlist = (arr) => {
     const delBtns = document.querySelectorAll('.remove-from-wishlist');
     delBtns.forEach(element => {
         element.addEventListener('click', () => {
-            const elementIndex = element.dataset.index;
+            const closest = element.closest(".product-card");
+            console.log(closest);
+
+            const elementId = closest.dataset.id;
+            console.log(elementId);
             arr = arr.filter(function (item) {
-                return item !== arr[elementIndex]
+                return item.id != elementId
             })
             wishlistItemContainer.innerHTML = ``;
             mapWishList(arr);
@@ -271,6 +275,8 @@ const mapWishList = (arr) => {
         arr.map((element, index) => {
             let data = document.createElement('tr');
             data.classList.add('data-row');
+            data.classList.add('product-card');
+            data.dataset.id = element.id
             // element.discount ? data.classList.add('indiscount') : '';
             // let discount = element.discount ? `
             //     <div class="card-disc">
@@ -282,7 +288,7 @@ const mapWishList = (arr) => {
             data.innerHTML = `
     <td class="product-remove">
     <div>
-    <a class="remove remove-from-wishlist" data-index="${index}" title="Remove this product">×</a>
+    <a class="remove remove-from-wishlist" title="Remove this product">×</a>
     </div>
     </td>
     <td class="product-thumbnail">
@@ -318,9 +324,10 @@ const mapWishList = (arr) => {
     }
     delItemWishlist(arr);
 }
-const localWishList = JSON.parse(localStorage.getItem('wishList'))
+
+// const localWishList = JSON.parse(localStorage.getItem('wishList'))
 if (wishlistItemContainer) {
-    mapWishList(localWishList);
+    mapWishList(wishList);
 }
 
 const countWishList = document.querySelector('.wishlist-count');
@@ -328,3 +335,122 @@ const updateWishlistCount = (count) => {
     countWishList.innerHTML = count;
 }
 updateWishlistCount(wishList.length);
+
+
+
+//     CART
+
+let cart = [{ ...products[0], numberOfUnits: 3 }];
+
+//    Add to cart
+const addToCartBtns = document.querySelectorAll('.addToCartBtn');
+addToCartBtns.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        const closest = e.target.closest(".swiper-slide");
+        const productId = closest.dataset.id;
+        const product = products.find(item => item.id == productId);
+        if (cart.some(item => item.id == productId)) {
+            cart.forEach(item => {
+                if (item.id == productId) {
+                    item.numberOfUnits = item.numberOfUnits + 1;
+                }
+            })
+        }
+        else {
+            cart.push({
+                ...product,
+                numberOfUnits: 1
+            })
+        }
+        console.log(cart);
+    })
+})
+
+const cartItemContainer = document.querySelector('.cart-items-wrapper');
+const mapCart = (arr) => {
+    cartItemContainer.innerHTML = ``;
+    if (arr.length > 0) {
+
+        arr.map((element) => {
+            let data = document.createElement('tr');
+            data.classList.add('data-row');
+            data.classList.add('product-card');
+            data.dataset.id = element.id;
+            // element.discount ? data.classList.add('indiscount') : '';
+            // let discount = element.discount ? `
+            //     <div class="card-disc">
+            //         <span>-${element.discount_percent}%</span>
+            //     </div>`  : ``;
+            // let oldPrice = element.discount ? `
+            //     <span class="old-price">${element.old_price}</span>` : ``;
+
+            data.innerHTML = `
+    <td class="product-remove">
+    <div>
+    <a class="remove remove-from-wishlist" data-id="${element.id}" title="Remove this product">×</a>
+    </div>
+    </td>
+    <td class="product-thumbnail">
+    <img src="./img/trend/${element.image}.jpg"
+    class="product-img" alt="product-image" loading="lazy"> </a>
+    </td>
+    <td class="product-name">
+    <a href="#">${element.name}</a>
+    <div class="view-box quick-view-btn" data-id="${element.id}" onClick="quickView(event)">
+    <span class="trendcard-add">view</span>
+    </div>
+    </td>
+    <td class="product-price">
+    <span class="price-txt">${element.current_price}</span>
+    </td>
+    <td class="product-quantity">
+    <div class="quantity-input">
+    <i class="fas fa-angle-up quantity-plus input-arrow" onClick="changeUnitsNumber('plus',${element.id})"></i>
+    <input type="number" class="input-text" step="1" min="1" max="${element.stock}" name="quantity" value="${element.numberOfUnits}"
+    size="3">
+    <i class="fas fa-angle-down quantity-minus input-arrow" onClick="changeUnitsNumber('minus', ${element.id})"></i>
+    </div>
+    </td>
+            `
+            cartItemContainer.appendChild(data);
+        })
+    }
+    else {
+        let data = document.createElement('p');
+        data.classList.add('data-row');
+        data.classList.add('empty-text');
+        data.innerHTML = `No products added to the wishlist`;
+        wishlistItemContainer.appendChild(data);
+        wishlistItemContainer.style.height = '200px';
+    }
+    // delItemWishlist(arr);
+}
+
+// const productQuantityInput = document.querySelector('.input-text');
+const changeUnitsNumber = (action, id) => {
+    cart = cart.map((item) => {
+        let numberOfUnits = item.numberOfUnits;
+
+        if (item.id == id) {
+            if (action == 'minus' && numberOfUnits > 1) {
+                numberOfUnits--;
+                console.log('a');
+            }
+            else if (action == 'plus' && numberOfUnits < item.stock) {
+                numberOfUnits++;
+                console.log('b');
+            }
+        }
+
+        return {
+            ...item,
+            numberOfUnits,
+        };
+    });
+
+    mapCart(cart);
+};
+
+if (cartItemContainer) {
+    mapCart(cart);
+};
